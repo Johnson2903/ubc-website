b24d8c10cf60a788ca4298f45776ce76b43cf551
Johnson2903 <49341589+Johnson2903@users.noreply.github.com>
2020-11-09 05:01:44 -0800

    Update config.php

--- ubc_admins/config/config.php
+++ ubc_admins/config/config.php
@@ -1,5 +1,10 @@
 <?php
-$conn = mysqli_connect('localhost','root', '', 'ubc');
+// Development mode
+// $conn = mysqli_connect('localhost','root', '', 'ubc');
+
+// Remote SQL
+$conn = mysqli_connect('remotemysql.com','CaSpnm3Y1a', '3UDHREcUba', 'CaSpnm3Y1a');
+
 if($conn){
      // echo "connect to db successful";
 }else{
